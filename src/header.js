667c44a7295af95ecf0628b13c0f6294435f96d4
octanym <camjmoore@outlook.com>
2023-05-18 23:29:15 -0400

    added ids and attempted controlled inputs for boolean checkbox with text value s in pizza form

--- src/header.js
+++ src/header.js
@@ -6,7 +6,10 @@ const Header = () => {
     <div className="flex flex-col justify-center items-center min-w-full min-h-[75vh] bg-header-image bg-cover">
       <h2 className="text-5xl">Your favorite food, delivered while coding</h2>
       <Link to="/pizza">
-        <button className="px-10 py-2 my-10 text-lg border border-black">
+        <button
+          className="px-10 py-2 my-10 text-lg border border-black"
+          id="order-pizza"
+        >
           Pizza?
         </button>
       </Link>
--- src/pizza.js
+++ src/pizza.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import Pizzajpg from "./Assets/Pizza.jpg";
 
 const Pizza = () => {
+  const [data, setData] = useState({
+    name: "",
+    size: "",
+    topping1: false,
+    topping2: false,
+    special: "",
+  });
+
+  const [toppings, setToppings] = useState({ topping1: "", topping2: "" });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+
+    /* const valid = type === "checkbox" ? checked : value; */
+
+    setToppings({ ...toppings, [name]: value });
+    /* setData({ ...data, [name]: valid }); */
+  };
+
   return (
     <div className="mx-auto my-5 max-w-lg border border-black">
       <div>
@@ -10,14 +29,17 @@ const Pizza = () => {
       </div>
       <div>
         <h3 className="px-5 py-5">Select Your Customizations</h3>
-        <form className="">
+        <form id="pizza-form">
           <div className="flex flex-col px-5 py-3 bg-gray-200">
             <label>Choice of Size</label>
             <small>Required</small>
           </div>
 
           <div className="px-5 py-3">
-            <select className="px-2 py-2 w-48 bg-white border border-black">
+            <select
+              className="px-2 py-2 w-48 bg-white border border-black"
+              id="size-dropdown"
+            >
               <option default>select</option>
               <option>Small</option>
               <option>Medium</option>
@@ -55,61 +77,61 @@ const Pizza = () => {
 
           <div className="flex flex-col flex-wrap max-h-52 px-5 py-3">
             <div className="w-42">
-              <input className="mr-2" type="checkbox" />
+              <input
+                className="mr-2"
+                type="checkbox"
+                checked={
+                  toppings.topping1 === "Pepperoni" ||
+                  toppings.topping2 === "Pepperoni"
+                }
+                value="Pepperoni"
+                name={toppings.topping1 ? "topping2" : "topping1"}
+                onChange={handleChange}
+              />
               <label>Pepperoni</label>
             </div>
 
             <div className="w-42">
-              <input className="mr-2" type="checkbox" />
+              <input
+                className="mr-2"
+                type="checkbox"
+                checked={
+                  toppings.topping1 === "Diced Tomatoes" ||
+                  toppings.topping2 === "Diced Tomatoes"
+                }
+                value="Diced Tomatoes"
+                name={toppings.topping1 ? "topping2" : "topping1"}
+                onChange={handleChange}
+              />
               <label>Diced Tomatoes</label>
             </div>
             <div className="w-42">
-              <input className="mr-2" type="checkbox" />
+              <input
+                className="mr-2"
+                type="checkbox"
+                checked={
+                  toppings.topping1 === "Sausage" ||
+                  toppings.topping2 === "Sausage"
+                }
+                value="Sausage"
+                name={toppings.topping1 ? "topping2" : "topping1"}
+                onChange={handleChange}
+              />
               <label>Sausage</label>
             </div>
             <div className="w-42">
-              <input className="mr-2" type="checkbox" />
+              <input
+                className="mr-2"
+                type="checkbox"
+                checked={
+                  toppings.topping1 === "Black Olives" ||
+                  toppings.topping2 === "Black Olives"
+                }
+                value="Black Olives"
+                name={toppings.topping1 ? "topping2" : "topping1"}
+                onChange={handleChange}
+              />
               <label>Black Olives</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Canadian Bacon</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Roasted Garlic</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Spicy Italian Sausage</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Artichoke Hearts</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Grilled Chicken</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Three Cheese</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Onions</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Pineapple</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Green Pepper</label>
-            </div>
-            <div className="w-42">
-              <input className="mr-2" type="checkbox" />
-              <label>Extra Cheese</label>
             </div>
           </div>
 
@@ -128,17 +150,27 @@ const Pizza = () => {
           <div className="px-5 py-3">
             <input
               className="w-96 px-3 py-3 border border-black"
+              id="special-text"
               type="text"
               placeholder="Anything else you'd like to add?"
             />
           </div>
 
-          <div className="flex px-5 py-3 border-t border-black">
+          <div className="flex flex-wrap px-5 py-3 border-t border-black">
             <input
-              className="w-20 mr-5 text-center border border-black"
+              className="w-96 px-3 py-1 mr-36 mb-2 border border-black"
+              id="name-input"
+              type="text"
+              placeholder="What's your name?"
+            />
+            <input
+              className="w-20 mr-4 text-center border border-black"
               type="number"
             />
-            <button className="flex justify-between w-72 px-3 py-1 border border-black">
+            <button
+              className="flex justify-between w-72 px-3 py-1 border border-black"
+              id="order-button"
+            >
               <p>Add to Order</p>
               <p>$17.99</p>
             </button>
